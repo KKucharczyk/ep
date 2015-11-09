bcb3af3f6d4984468b4fd39c9d7b9b008f5dbf2b
Krzysztof Kucharczyk <toficzak@gmail.com>
2015-11-09 13:44:00 +0100

    Prototype of mixing overworld and battle screen. Time to go from this further.

---
+++ Scavenger/Assets/Scripts/Overworld/Spawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	IEnumerator OnTriggerEnter2D(Collider2D other) {
+
+		ScreenFader sf = GameObject.FindGameObjectWithTag ("Fader").GetComponent<ScreenFader>();
+
+		if (other.tag == "Player") {
+			Debug.Log ("Detected in spawn area.");
+			if(countEncounterChance() == true) {
+				yield return StartCoroutine(sf.FadeToBlack());
+				Application.LoadLevel("Main");
+			}
+		
+		}
+
+	}
+
+	private bool countEncounterChance() {;
+		int chance = Random.Range (1,10);
+		Debug.Log ("Encounter chance: " + chance);
+		if (chance == 9)
+			return true;
+		else
+			return false;
+	}
+}
